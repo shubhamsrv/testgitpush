c3d63b8bb67e468983e3ddedc2f6cb1c5419d216
shubham <shubham.srv10@gmail.com>
2022-07-29 18:21:47 +0530

    this is my first code push in git fsfsss

--- mongotest.py
+++ mongotest.py
@@ -4,9 +4,9 @@ db = client.test
 print(db)
 
 d = {
-    "name":"sudhanshu",
-    "email":"sudhanshu@ineuron.ai",
-    "surname":"kumar"
+    "name":"shubham",
+    "email":"shubham.srv10@gmail.com",
+    "surname":"srivastav"
      }
 db1 = client['mongotest']
 coll = db1['test']
